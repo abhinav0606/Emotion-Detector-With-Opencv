7bf037cb4fb965f067cd53ab6330f2c19c21679a
Abhinav Gangrade <48849486+abhinav0606@users.noreply.github.com>
2020-12-11 17:30:57 +0530

    Completed

--- camera.py
+++ camera.py
@@ -1,10 +1,57 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.preprocessing.image import ImageDataGenerator
 import cv2
 import numpy as np
-from model_return import retun_model
-model=retun_model().load_weights("model1.h5")
+# directory
+train="train"
+test="test"
+# data generator
+test_data_generator=ImageDataGenerator(1./255)
+train_data_generator=ImageDataGenerator(1./255)
+# creating the generator
+test_generator=test_data_generator.flow_from_directory(
+    test,target_size=(48,48),batch_size=64,color_mode="grayscale",
+    class_mode="categorical"
+)
+train_generator=train_data_generator.flow_from_directory(
+    train,target_size=(48,48),batch_size=64,color_mode="grayscale",
+    class_mode="categorical"
+)
+
+emotion=Sequential()
+emotion.add(Conv2D(32,input_shape=(48,48,1),kernel_size=(3,3),activation="relu"))
+emotion.add(Conv2D(64,kernel_size=(3,3),activation="relu"))
+emotion.add(MaxPooling2D(pool_size=(2,2)))
+emotion.add(Dropout(0.25))
+emotion.add(Conv2D(128,kernel_size=(3,3),activation="relu"))
+emotion.add(MaxPooling2D(pool_size=(2,2)))
+emotion.add(Conv2D(128,kernel_size=(3,3),activation="relu"))
+emotion.add(MaxPooling2D(pool_size=(3,3)))
+emotion.add(Dropout(0.25))
+emotion.add(Flatten())
+emotion.add(Dense(1024,activation="relu"))
+emotion.add(Dropout(0.5))
+emotion.add(Dense(7,activation="softmax"))
+emotion.load_weights("model1.h5")
+import cv2
+import numpy as np
 capture=cv2.VideoCapture(0)
 faces=cv2.CascadeClassifier("frontal_face.xml")
 cv2.ocl.setUseOpenCL(False)
+Emotion_dictionary={
+    0:"Angry",
+    1:"Disgusted",
+    2:"Fearfull",
+    3:"Happy",
+    4:"Neutral",
+    5:"Sad",
+    6:"Surprised"
+}
 while True:
     ret,frame=capture.read()
     frame=cv2.flip(frame,1)
@@ -14,7 +61,11 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),[0,255,0],2)
         cropped_gray_image=gray_frame[y:y+h,x:x+w]
         new_size_frame=np.expand_dims(np.expand_dims(cv2.resize(cropped_gray_image,(48,48)),-1),0)
-        cv2.imshow("Abhinav's Frame",cropped_gray_image)
+        prediction=emotion.predict(new_size_frame)
+        index=int(np.argmax(prediction))
+        cv2.putText(frame, Emotion_dictionary[index], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+    cv2.imshow("Abhinav's Frame",frame)
     if cv2.waitKey(1)==13:
         break
 capture.release()
